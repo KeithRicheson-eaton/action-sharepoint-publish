1d77c39ad0adcacd65d9fc3b4eecbe634a110c29
Keith Richeson <149623270+KeithRicheson-eaton@users.noreply.github.com>
2024-04-26 13:27:57 -0400

    Add glob support (#1)
    * Add base_path input, clarify file_path usage
    
    * Replace file content options with glob options
    
    * Replace javascript with valid shell script
    
    * Try quoting the replacement
    
    * Fix outer quoting
    
    * Quote substitution pattern
    
    * Try two steps and no quotes
    
    * Combine substitution
    
    * use list for file_path
    
    * Add some debug output
    
    * Adding/fixing debug
    
    * Prevent full expanion in test
    
    * Remove debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -l
 
 # if the FILE_PATH exists then we will just be uploading the file
-if [[ -z $FILE_PATH ]]
+if [[ -z "$FILE_PATH" ]]
 then
-    export FILE_PATH="/out/repoarchive.zip"
+    sha=${GITHUB_SHA:0:7}
+    fileName=${GITHUB_REPOSITORY//\//_}_${sha}_.zip
+
+    export FILE_PATH="/out/${fileName}"
     mkdir /out
     cd "$GITHUB_WORKSPACE"
     echo "Creating archive";
@@ -13,4 +16,3 @@ fi
 # run the script to send to sharepoint
 node /app/index.js
 [ $? -eq 0 ]  || exit 1
-
